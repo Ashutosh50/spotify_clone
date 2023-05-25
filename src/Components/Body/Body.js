c057c6274c86b60eb89b53464bacbd217ce1552b
Ashutosh50 <ashutoshsahu507@gmail.com>
2023-05-26 02:25:28 +0530

    Working

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Body.css'
+import Header from '../Header/Header'
+import { useStateValue } from '../Others/Datalayer';
 
-const Body = () => {
+const Body = ({spotify}) => {
+  const [{ discover_weekly }] = useStateValue();
   return (
     <div className='body'>
-      Body
+       <Header spotify={spotify} />
+       <div className="body__info">
+        <img src={discover_weekly?.images[0].url} alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+        <div className='body_songs'>
+           <div className='body_icons'>
+            
+           </div>
+        </div>
       </div>
   )
 }
